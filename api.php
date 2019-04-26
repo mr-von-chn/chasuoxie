de916786bb8f090a5a1c30118afa0c689f5848f2
mr-von-chn <mr_von_chn@sina.com>
2019-04-26 12:56:29 +0800

    Update api.php

--- api.php
+++ api.php
@@ -8,23 +8,61 @@
 		if (!$conn) {
 		    die("Connection failed: " . mysqli_connect_error());
 		}
-		$sql = "SELECT count(1) as total FROM `short-words`.words";
+		$sql = "SELECT SQL_CACHE count(1) as total FROM `short-words`.words WHERE `status`=1";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 		    // 输出数据
 		    $row = $result->fetch_assoc();
 			$conn->close();
-	    	echo json_encode(['error'=>0, 'msg'=>'', 'data'=>$row['total']], true);
+	    	echo json_encode(['error'=>0, 'msg'=>'ok', 'data'=>$row['total']], true);
 	    	exit;
 		} else {
 			$conn->close();
-	    	echo json_encode(['error'=>0, 'msg'=>'', 'data'=>[]]);
+	    	echo json_encode(['error'=>1, 'msg'=>'查询总数出错', 'data'=>[]]);
 	    	exit;
 		}
 	} else {
-		//查缩写词
 		$short = $_GET['short']??$_POST['short']??'';
-		if (!empty($short)) {
+		$full = $_GET['full']??$_POST['full']??'';
+		$desc = $_GET['desc']??$_POST['desc']??'';
+		$author = $_GET['author']??$_POST['author']??'';
+
+		//添加词条
+		if (!empty($short)&&!empty($full)&&!empty($desc)) {
+			$short = htmlspecialchars(stripslashes(strip_tags(trim($short))));
+			$lower = strtolower($short);
+			$full = htmlspecialchars(stripslashes(strip_tags(trim($full))));
+			$desc = htmlspecialchars(stripslashes(strip_tags(trim($desc))));
+			$author = htmlspecialchars(stripslashes(strip_tags(trim($author))));
+			
+			// 创建连接
+			$conn = mysqli_connect('localhost', 'root', '12345678');
+			 
+			// 检测连接
+			if (!$conn) {
+			    die("Connection failed: " . mysqli_connect_error());
+			}
+			//查询词条是否已存在
+			$sql = "SELECT SQL_CACHE * FROM `short-words`.words WHERE `lower`='{$lower}' LIMIT 1";
+			$result = $conn->query($sql);
+			if ($result->num_rows > 0) {
+		    	echo json_encode(['error'=>1, 'msg'=>'词条已存在', 'data'=>[]], true);
+		    	exit;
+			}
+
+			//插入词条
+			$sql = "INSERT INTO `short-words`.words (`short`, `lower`, `full`, `desc`, `author`) VALUES ('{$short}', '{$lower}', '{$full}', '{$desc}', '{$author}')";
+			if ($conn->query($sql) === TRUE) {
+				$conn->close();
+		    	echo json_encode(['error'=>0, 'msg'=>'插入成功，审核通过后即可查询'], true);
+		    	exit;
+			} else {
+				$conn->close();
+	    		echo json_encode(['error'=>1, 'msg'=>'插入词条出错', 'data'=>[]]);
+		    	exit;
+			}
+		} elseif (!empty($short)) {
+			//查缩写词
 			$short = htmlspecialchars(stripslashes(strip_tags(strtolower(trim($short)))));
 			// 创建连接
 			$conn = mysqli_connect('localhost', 'root', '12345678');
@@ -34,7 +72,7 @@
 			    die("Connection failed: " . mysqli_connect_error());
 			}
 
-			$sql = "SELECT SQL_CACHE * FROM `short-words`.words WHERE `lower`='{$short}' LIMIT 1";
+			$sql = "SELECT SQL_CACHE * FROM `short-words`.words WHERE `lower`='{$short}' AND `status`=1 LIMIT 1";
 			$result = $conn->query($sql);
 			if ($result->num_rows > 0) {
 			    // 输出数据
@@ -44,7 +82,7 @@
 		    	exit;
 			} else {
 				$conn->close();
-		    	echo json_encode(['error'=>0, 'msg'=>'', 'data'=>[]]);
+		    	echo json_encode(['error'=>0, 'msg'=>'词条不存在']);
 		    	exit;
 			}
 		} else {
